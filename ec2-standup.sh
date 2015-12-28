c60610e9a930462ad78215eef2216508419a1e23
Brian Highnam <bhighnam@gmail.com>
2015-12-28 11:58:56 -0500

    updated the standup shell script and created ebsMapping.json file

--- ec2-standup.sh
+++ ec2-standup.sh
@@ -6,3 +6,27 @@
 
 # All of the variables needed to standup the EC2 instances
 
+AMI="ami-60b6c60a" # Amazon Linux AMI 2015.09.01
+INSTANCE_TYPE="t2.micro" # free tier eligble  instance
+KEY_PAIR="dev-box-key"
+SECURITY_GROUPS="sg-f09a6a96"
+AZ="us-east-1a"
+SUBNET_ID="subnet-921fc1b9"
+
+# kicking off the automated-stand up job for ec2-instance
+
+echo "*************** STANDING UP EC2 INSTANCE ****************************"
+
+ aws ec2 run-instances --image-id "$AMI" \
+	--key-name "$KEY_PAIR" \
+	--security-group-ids "$SECURITY_GROUPS" \
+	--instance-type "$INSTANCE_TYPE" \
+	--subnet-id "$SUBNET_ID" \
+	--count "1" \
+	--instance-initiated-shutdown-behavior "terminate" \
+	--block-device-mapping "file:////home/brian/sandbox/automate-ec2-standup/ebsMapping.json" > output.txt
+
+echo "*************** END OF STANDING UP EC2 INSTANCE ****************************"
+
+
+
