4ace5b6e59ba5be735314c0870dbe78ac80f8af6
brian.highnam <brianhighnam@brianhihnamsMBP.home>
2016-01-09 14:21:58 -0500

    updated

--- ec2-standup.sh
+++ ec2-standup.sh
@@ -8,7 +8,7 @@
 
 AMI="ami-60b6c60a" # Amazon Linux AMI 2015.09.01
 INSTANCE_TYPE="t2.micro" # free tier eligble  instance
-KEY_PAIR="dev-box-key"
+KEY_PAIR="macbook-key"
 SECURITY_GROUPS="sg-f09a6a96"
 AZ="us-east-1a"
 SUBNET_ID="subnet-921fc1b9"
@@ -29,9 +29,9 @@ echo "*************** STANDING UP EC2 INSTANCE ****************************"
 	--instance-type "$INSTANCE_TYPE" \
 	--subnet-id "$SUBNET_ID" \
 	--count "1" \
-	--user-data "file:////home/brian/sandbox/automate-ec2-standup/bootstrap_apache.sh" \
+	--user-data "file:////Users/brianhighnam/Documents/sandbox/automate-ec2-standup/bootstrap_apache.sh" \
 	--instance-initiated-shutdown-behavior "terminate" \
-	--block-device-mapping "file:////home/brian/sandbox/automate-ec2-standup/ebsMapping.json" > output.txt
+	--block-device-mapping "file:////Users/brianhighnam/Documents/sandbox/automate-ec2-standup/ebsMapping.json" > output.txt
 
 echo "*************** END OF STANDING UP EC2 INSTANCE ****************************"
 
@@ -42,5 +42,5 @@ echo "The instance that was spun up was ${INSTANCE_ID}"
 
 # Slaps on the tag for the instance to be used
 aws ec2 create-tags --resources "${INSTANCE_ID}" \
-	--tags "file:////home/brian/sandbox/automate-ec2-standup/ec2Tags.json" 
+	--tags "file:////Users/brianhighnam/Documents/sandbox/automate-ec2-standup/ec2Tags.json" 
 
